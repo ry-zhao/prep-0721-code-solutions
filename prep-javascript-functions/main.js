fdf8d271d80baa9a8543d0c6de4284b1e2e529a5
Ryan Zhao <ryanzhao21@gmail.com>
2021-08-10 20:45:53 -0700

    Completed convertHoursToMinutes()

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -4,3 +4,10 @@ function addTwoNumbers(num1, num2) {
 
 var sum = addTwoNumbers(2, 2);
 console.log('addTwoNumbers(2, 2):', sum);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var min = convertHoursToMinutes(24);
+console.log('convertHoursToMinutes(24):', min);
