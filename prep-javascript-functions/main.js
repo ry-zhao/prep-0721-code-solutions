48f81cbd0caefc1e6096d3e5fb245fc3194695aa
Ryan Zhao <ryanzhao21@gmail.com>
2021-08-10 20:53:22 -0700

    Completed subtractTwoNumbers()

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -8,7 +8,6 @@ console.log('addTwoNumbers(2, 2):', sum);
 function convertHoursToMinutes(hours) {
   return hours * 60;
 }
-
 var min = convertHoursToMinutes(24);
 console.log('convertHoursToMinutes(24):', min);
 
@@ -17,3 +16,15 @@ function getGreeting(name) {
 }
 var world = getGreeting('World');
 console.log('getGreeting("World"):', world);
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+var number = addAndMultiplyBy5(10, 10);
+console.log('addAndMultiplyBy5(10, 10):', number);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+var subtract = subtractTwoNumbers(100, 75);
+console.log('subtractTwoNumbers(100, 75):', subtract);
